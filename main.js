509755d9f28cf7674b98a3616c0b7391262efb0c
Arjun Vijay <arjunvijay2002113@gmail.com>
2023-05-02 14:07:25 +0530

    made some changes in design

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 var data = {};
 const adviceId = document.querySelector("#advice-id");
 const advice = document.querySelector("#advice");
-const button = document.querySelector("#btn")  
-fetch("https://api.adviceslip.com/advice")
-.then(res => res.json() )
-.then(json =>  data = json )
-.then(() =>{
-    var resetAdvice = () => {
-        adviceId.innerHTML = data.slip.id;
-        advice.innerHTML = `"${data.slip.advice}"`;
-    }
-    console.log(data);
-    resetAdvice();
-    button.addEventListener("click", () =>{
-        location.reload();
-    })
-})
+const button = document.querySelector("#btn");
+
+const fetchData = () => {
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((json) => (data = json))
+    .then(() => {
+      adviceId.innerHTML = data.slip.id;
+      advice.innerHTML = `"${data.slip.advice}"`;
+      
+    });
+};
+fetchData();
+
+button.addEventListener("click", fetchData);
